702b3a9ec5a5bf6331b231744c23b681847e368a
Taha <taha.adse@gmail.com>
2024-08-30 17:04:44 +0100

    make main a module

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import myPlugin from './custom-plugin';
+import Inspect from 'vite-plugin-inspect'
 
 export default defineConfig({
-  plugins: [myPlugin()]
+  plugins: [myPlugin(), Inspect()]
 });
 
